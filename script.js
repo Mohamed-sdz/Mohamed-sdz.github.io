c4e4d463aaa2aad5dc0939325ee00f5bc3eff5e8
saiderrahmani <didi.mousedidi@outlook.com>
2023-05-14 08:33:57 -0400

    script.js

--- script.js
+++ script.js
@@ -22,3 +22,36 @@ navLinks.forEach(link => {
    const emailInput = document.querySelector('#email');
    const messageInput = document.querySelector('#message');
    const errorContainer = document.querySelector('#error-msg');
+ 
+   form.addEventListener('submit', (event) => {
+    let errorMessages = [];
+
+     // Check if name input is empty
+       if (nameInput.value === '') {
+     errorMessages.push('Name is required');
+}
+// Check if email input is empty and valid
+if (emailInput.value === '') {
+    errorMessages.push('Email is required');
+  } else if (!isValidEmail(emailInput.value)) {
+    errorMessages.push('Please enter a valid email address');
+  }
+  // Check if message input is empty
+  if (messageInput.value === '') {
+    errorMessages.push('Message is required');
+  }
+
+  // Display error messages if any exist
+  if (errorMessages.length > 0) {
+    event.preventDefault(); // Prevent form submission
+
+    const errorHTML = errorMessages.map(msg => `<li>${msg}</li>`).join('');
+    errorContainer.innerHTML = `<ul>${errorHTML}</ul>`;
+  }
+});
+
+// Helper function to validate email address
+function isValidEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}

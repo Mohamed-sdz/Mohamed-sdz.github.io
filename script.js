e4dc14d3e80a20fd75b36d9433b729a11c1e6c47
saiderrahmani <didi.mousedidi@outlook.com>
2023-05-14 07:22:18 -0400

    script.js

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 // select all the navigation links
 const navLinks = document.querySelectorAll('nav ul li a');
+
+ // add click event listener to each navigation link
+navLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+      // prevent default jump-to-section behavior
+        event.preventDefault();
+  
+      // get the target section from the link href attribute
+      const targetId = link.getAttribute('href');   
+  
+      // smoothly scroll to the target section using scrollIntoView method
+      document.querySelector(targetId).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
